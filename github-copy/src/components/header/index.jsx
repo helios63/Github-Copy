4b885716fde8d8494fe65947f7e75cb2372b1a28
Maxime Mondet <maxime.mondet@gmail.com>
2021-12-14 17:01:00 +0100

    adding Header (Logo + search bar + avatar)

--- github-copy/src/components/header/index.jsx
+++ github-copy/src/components/header/index.jsx
@@ -1,8 +1,16 @@
-import {Header, Logo} from './styles.js';
+import {Header, Logo, Avatar} from './styles.js';
+import logo from "./logo.png";
+import avatar from "./avatar.png";
 
 const HeaderComponent = () => (
   <Header>
-    <Logo title="Logo Github" />
+    <div class="flex">
+      <Logo src={logo} title="Logo Github" />
+      <input type="text" name="search" placeholder='Search or jump to...'/>
+    </div>
+    <div class="flex">
+      <Avatar src={avatar} title="User avatar"  />
+    </div>
   </Header>
 )
 
--- github-copy/src/components/header/styles.js
+++ github-copy/src/components/header/styles.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components'
 
-export const Logo = styled.div`
+export const Logo = styled.img`
   height: auto;
   max-width: 40px;
   width: 100%;
 `
 export const Header = styled.header`
+  background-color: #F3F0F0;
   display: flex;
   justify-content: space-between;
   padding: 1em;
+`
+export const Avatar = styled.img`
+  height: auto;
+  max-width: 40px;
+  width: 100%;
 `
