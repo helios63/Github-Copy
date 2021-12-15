645cfec0da41c161234437c47c0009b15b3ec4a3
Maxime Mondet <maxime.mondet@gmail.com>
2021-12-15 16:22:55 +0100

    Fixing Header component

--- github-copy/src/components/header/index.jsx
+++ github-copy/src/components/header/index.jsx
@@ -1,17 +1,24 @@
-import {Header, Logo, Avatar} from './styles.js';
+import React from 'react'
+import {Header, Logo, Avatar, Nav} from './styles.js';
 import logo from "./logo.png";
 import avatar from "./avatar.png";
 
 const HeaderComponent = () => (
   <Header>
-    <div class="flex">
+    <div class="flex items-center">
       <Logo src={logo} title="Logo Github" />
-      <input type="text" name="search" placeholder='Search or jump to...'/>
+      <input class="mr-4" type="text" name="search" placeholder='Search or jump to...'/>
+      <Nav>
+        <a href="#">Pull Requests</a>
+        <a href="#">Issues</a>
+        <a href="#">Marketplace</a>
+        <a href="#">Explore</a>
+      </Nav>
     </div>
     <div class="flex">
-      <Avatar src={avatar} title="User avatar"  />
+      <Avatar src={avatar} title="User avatar" />
     </div>
   </Header>
 )
 
-export default HeaderComponent;
+export default HeaderComponent
--- github-copy/src/components/header/styles.js
+++ github-copy/src/components/header/styles.js
@@ -4,15 +4,22 @@ export const Logo = styled.img`
   height: auto;
   max-width: 40px;
   width: 100%;
+  margin-right: 0.8em;
 `
 export const Header = styled.header`
   background-color: #F3F0F0;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 1em;
 `
 export const Avatar = styled.img`
   height: auto;
   max-width: 40px;
   width: 100%;
+`
+export const Nav = styled.nav`
+  width: 320px;
+  display:flex;
+  justify-content: space-between;
 `
